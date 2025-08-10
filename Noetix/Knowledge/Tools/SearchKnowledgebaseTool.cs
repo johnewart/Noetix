6cba4aa5d010d359a4fab5681b86b922f490b78f
John Ewart <john@johnewart.net>
2025-08-09 23:39:26 -0700

    Hack...

--- Noetix/Knowledge/Tools/SearchKnowledgebaseTool.cs
+++ Noetix/Knowledge/Tools/SearchKnowledgebaseTool.cs
@@ -17,14 +17,14 @@ public class SearchKnowledgebaseOutput(List<Document> documents)
 public class SearchKnowledgebaseTool(Knowledgebase knowledgeBase) : AssistantTool
 {
     public override string Id => "SearchKnowledgebase";
-    public override string Purpose => "Search the Knowledgebase";
+    public override string Purpose => "Search the knowledge base for documents";
 
     public override ToolResultsSchema Results => new(typeof(SearchKnowledgebaseOutput));
     public override ToolParamsSchema Parameters => new(typeof(SearchKnowledgebaseInput));
 
     public override string Description => @"
-            Searches the Knowledgebase for documents matching the specified query, returning up to the specified limit.
-            If you need to find information on a specific topic, you can use this tool to search the Knowledgebase.
+            Searches the knowledge base for documents matching the specified query, returning up to the specified limit.
+            If you need to find information on a specific topic, you can use this tool to search the knowledge base.
             You can invoke this tool a number of times to search for different topics or to refine your search.
         ";
 
